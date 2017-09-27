ac9a55fe299d365848f06f087005cf37596f3ebd
Amanda Lilley <yoknitting24@gmail.com>
2017-09-27 14:12:00 -0400

    Add callback

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,34 +1,51 @@
-alert("Why hello there. Just testing you're awake!");
-var animatePoints = function() {
+var icons = document.getElementsByClassName('ion');
 
-  var points = document.getElementsByClassName('point');
-
-  var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  for (var i = 0; i < points.length; i++) {
-  revealPoint(i);
-  }
+var spinMe = function(ion) {
+  ion.style.transition = "all 3s";
+ion.style.transform = "rotate(360deg)";
+ion.style.msTransform = "rotate(360deg)";
+  ion.style.WebkitTransform = "rotate(360deg)";
 };
 
-animatePoints();
-
-var spinIcons = function(){
-  var icons = document.getElementsByClassName('ion');
-
-  var spinMe = function(index){
-    icons[index].style.transition = "all 3s";
-    icons[index].style.transform = "rotate(360deg)";
-    icons[index].style.msTransform = "rotate(360deg)";
-    icons[index].style.WebkitTransform = "rotate(360deg)";
-  };
-
-  for (var i=0; i < icons.length; i++) {
-  spinMe(i);
-  }
+var spinIcons = function(icons) {
+  forEach(icons, spinMe);
 };
-spinIcons();
+
+window.onload = function() {
+
+  if (window.innerHeight > 950) {
+        spinIcons(i);
+    }
+    // var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    // var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener("scroll", function(event) {
+      if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        spinIcons(icons);
+      }
+    });
+  }
+
+
+
+
+
+
+
+    // var animatePoints = function() {
+    //
+    //   var points = document.getElementsByClassName('point');
+    //
+    //   var revealPoint = function(index) {
+    //     points[index].style.opacity = 1;
+    //     points[index].style.transform = "scaleX(1) translateY(0)";
+    //     points[index].style.msTransform = "scaleX(1) translateY(0)";
+    //     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    //   };
+    //
+    //   for (var i = 0; i < points.length; i++) {
+    //   revealPoint(i);
+    //   }
+    // };
+
+    // animatePoints();
