04a5491af60383b89a141551feb9a0e155820786
Amanda Lilley <yoknitting24@gmail.com>
2017-09-30 11:12:07 -0400

    Update landing JS

--- scripts/landing.js
+++ scripts/landing.js
@@ -13,11 +13,11 @@ var spinIcons = function(icons) {
 
 window.onload = function() {
 
-  if (window.innerHeight > 950) {
-        spinIcons(i);
-    }
-    // var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    // var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  // if (window.innerHeight > 950) {
+  //       spinIcons(i);
+  //   }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
     window.addEventListener("scroll", function(event) {
       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
