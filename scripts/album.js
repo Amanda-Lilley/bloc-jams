e68f7b6987bff411662e29bfab70d90a6e4275ae
Amanda Lilley <yoknitting24@gmail.com>
2017-10-02 21:06:20 -0400

    Refactor Collection and Album Views

--- scripts/album.js
+++ scripts/album.js
@@ -94,28 +94,39 @@ var createSongRow = function(songNumber, songName, songLength) {
     '  <td class="song-item-duration">' + songLength + '</td>' +
     '</tr>';
 
-  return template;
+  return $(template);
 };
 
-var albumTitle = document.getElementsByClassName('album-view-title')[0];
-var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-var albumImage = document.getElementsByClassName('album-cover-art')[0];
-var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+// var albumTitle = document.getElementsByClassName('album-view-title')[0];
+// var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+// var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+// var albumImage = document.getElementsByClassName('album-cover-art')[0];
+// var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var $albumTitle = $('.album-view-title');
+  var $albumArtist = $('.album-view-artist');
+  var $albumReleaseInfo = $('.album-view-release-info');
+  var $albumImage = $('.album-cover-art');
+  var $albumSongList = $('.album-view-song-list');
 
 var setCurrentAlbum = function(album) {
+      $albumTitle.text(album.title);
+       $albumArtist.text(album.artist);
+       $albumReleaseInfo.text(album.year + ' ' + album.label);
+       $albumImage.attr('src', album.albumArtUrl);
 
-  albumTitle.firstChild.nodeValue = album.title;
-  albumArtist.firstChild.nodeValue = album.artist;
-  albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-  albumImage.setAttribute('src', album.albumArtUrl);
+  // albumTitle.firstChild.nodeValue = album.title;
+  // albumArtist.firstChild.nodeValue = album.artist;
+  // albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+  // albumImage.setAttribute('src', album.albumArtUrl);
 
   // #3
-  albumSongList.innerHTML = '';
+  $albumSongList.empty();
 
   // #4
   for (var i = 0; i < album.songs.length; i++) {
-    albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         $albumSongList.append($newRow);
   }
 };
 
@@ -182,7 +193,7 @@ window.onload = function() {
   var albums = [albumPicasso, albumMarconi, albumSublime];
   var index = 1;
 
-  albumImage.addEventListener('click', function(event) {
+  $albumImage.click(function(event) {
     setCurrentAlbum(albums[index]);
     index++;
     if (index == albums.length) {
